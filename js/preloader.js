aba9e8e369ec1f2bae8ab0f91ef4164b0844751e
xayyam88 <xalid-7@mail.ru>
2019-09-20 14:39:57 +0400

    intial commit

---
+++ js/preloader.js
@@ -0,0 +1,7 @@
+window.onload = function() {  
+    let preloader = document.getElementById('preloader'); 
+    function foo() {
+       preloader.style.display = 'none';
+    }
+    setInterval(foo, 3000);
+}
